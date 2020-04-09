b7edb0c722927772c97d158ff0eb5e81c328e7d7
cogentbatch5series <cogentbatch@gmail.com>
2020-04-09 09:29:16 -0400

    7th commit

--- src/main/java/com/saran/controller/SaranController.java
+++ src/main/java/com/saran/controller/SaranController.java
@@ -1,13 +1,19 @@
 package com.saran.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class SaranController {
 	@GetMapping("/")
 	public String getMessage() {
-		return "Welcome to SpgBootGitJenkinsIntegration";
+		return "Welcome to GitAWSCodeDeploy";
+	}
+	@GetMapping("/")
+	public String getAWS() {
+		return "Welcome to AWS";
 	}
 
 }
